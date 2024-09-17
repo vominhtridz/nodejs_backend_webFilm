c4705238ef47b5c4f93281eefad2edd119fd863e
vominhtridz <trilove156@gmail.com>
2024-09-17 09:55:13 +0700

    1

--- config/allowOrigin.js
+++ config/allowOrigin.js
@@ -1,6 +1,7 @@
 export const AllowOrigin = [
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
   "http://localhost:5173",
   "https://frontend-react-webfilm.onrender.com",
+  "https://pagead2.googlesyndication.com",
+  "https://connect.facebook.net",
+  "https://search.google.com",
 ];
